2e1a338a7c81e47e868237d621b8602953265b4d
GeraldOrzal <46020310+GeraldOrzal@users.noreply.github.com>
2021-06-20 11:10:42 +0800

    Update AuthProvider.js

--- src/view/AuthProvider.js
+++ src/view/AuthProvider.js
@@ -25,7 +25,7 @@ export default function AuthProvider({children}) {
             }
             
             setDetails(UserDetails)
-        
+            console.log(UserDetails)
             
         }
         
